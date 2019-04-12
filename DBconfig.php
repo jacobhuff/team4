851936c7ad4202fa0b667858c6ada686448b5e42
Jonathan Cano <39822627+John1497@users.noreply.github.com>
2019-04-11 22:47:42 -0400

    Add files via upload

---
+++ DBconfig.php
@@ -0,0 +1,15 @@
+<?php
+
+//Define your host here.
+$HostName = "deltav-devtest.ci5dqgpsfw9c.us-east-2.rds.amazonaws.com";
+ //$HostName = 'localhost';
+//Define your database name here.
+$DatabaseName = "DeltaV_Test_Database_1";
+
+//Define your database username here.
+$HostUser = "DeltaV499s18";
+
+//Define your database password here.
+$HostPass = "CS499UK!";
+
+?>
